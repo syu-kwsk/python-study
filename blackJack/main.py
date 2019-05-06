35b5210bed2bf4b67e790e527504461c8f3332cb
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-05-06 23:58:47 +0900

    add branching

--- blackJack/main.py
+++ blackJack/main.py
@@ -7,7 +7,16 @@ print('私のアップカードは', upCardNum, 'です')
 userCard1 = random.randrange(1, 14)
 userCard2 = random.randrange(1, 14) 
 print('あなたの手札：', userCard1, userCard2)
+userCardSum = userCard1 + userCard2
 
 userChoice = input('"stand"しますか？"hit"しますか？:')
+
+if userChoice == "s" or userChoice == "stand":
+    print("stand")
+elif userChoice == "h" or userChoice == "hit":
+    print(userCardSum)
+    
+    
+    
 
 
