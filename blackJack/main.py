c8aba6d034296bab53ee4c7012bdecd4c4dd4aa2
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-05-06 23:38:54 +0900

    add introduction

--- blackJack/main.py
+++ blackJack/main.py
@@ -2,6 +2,12 @@
 tramp = []
 for number in range(13):
     tramp.append(number)
-    print(tramp[number])
 
+upCardNum = 1 #ランダムな一枚にしたい
 
+print('私のアップカードは', upCardNum, 'です')
+userCard1 = 10
+userCard2 = 11 
+print('あなたの手札：', userCard1, userCard2)
+
+userChoice = input('"stand"しますか？"hit"しますか？:')
