4bb67227a8fdc4a05e88fcf18d59baf14fb37bdb
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-05-07 01:23:21 +0900

    modify replyMessage

--- blackJack/main.py
+++ blackJack/main.py
@@ -19,48 +19,62 @@ print('あなたの手札：', userCard1, userCard2, '\n')
 
 userCardSum = userCard1 + userCard2
 
-while True:    
-    userChoice = input('"stand"しますか？"hit"しますか？:')
-    thinkingTimer(2)
-
-    if userChoice == "s" or userChoice == "stand":
-         print('スタンドします\n')
-         break
-    elif userChoice == "h" or userChoice == "hit":
+while True: 
+    userChoice = input('"stand"しますか？"hit"しますか？:')   
+         
+    if userChoice == "h" or userChoice == "hit":
          hitCard =  random.randrange(1, 14)
+         thinkingTimer(2)
          print('\nヒットしたカードは', hitCard, 'でした' )
          userCardSum += hitCard
          print('現在の手札の合計は', userCardSum, 'です\n')
+          
+    elif userChoice == "s" or userChoice == "stand":
+         print('スタンドします\n')
+         thinkingTimer(1)
 
-if userCardSum >= 22:
-    print('バーストですね. あなたの負け確定です')
-
-else:
-    print('それで来ましたか. 私の番ですね\n')
-    thinkingTimer(2)
-    
-    while True:
-         dealHitNum = random.randrange(1, 14)
-         dealCardSum += dealHitNum
-
-         print(dealHitNum, 'でした')
-         print('合計は', dealCardSum, 'です\n')
-         thinkingTimer(2)
-         
-         if dealCardSum <= 16:
-             print('ヒットします')
-         elif dealCardSum > 21:
-             print('バーストですね')
+         if userCardSum >= 22:
+             thinkingTimer(1)
+             print('バーストですね. あなたの負けです')
              break
-         else:
-             print('それではいきます')
-             print('')
-             if dealCardSum < userCardSum:
-                 print('あなたの勝ちです')
+         
+         print('それで来ましたか. 私の番ですね\n')
+         print('アップカードは', dealCardSum, 'でした')
+         thinkingTimer(2)
+    
+         while True:
+             dealHitNum = random.randrange(1, 14)
+             dealCardSum += dealHitNum
+
+             print('引きます')
+             thinkingTimer(1)
+             print(dealHitNum, 'でした')
+             print('合計は', dealCardSum, 'です\n')
+             thinkingTimer(2)
+
+             if dealCardSum <= 16:
+                 print('ヒットします')
+                 thinkingTimer(1)
+
+             elif dealCardSum > 21:
+                 print('バーストですね. あなたの勝ちです')
+                 thinkingTimer(1)
                  break
-             elif dealCardSum >= userCardSum:
-                 print('あなたの負けです')
-                 break
+             else:
+                 print('それではいきます')
+                 thinkingTimer(1)
+                 print('私は　', dealCardSum, '　で、あなたは', userCardSum, '　です')
+                 thinkingTimer(1)
+                 if dealCardSum < userCardSum:
+                     print('あなたの勝ちです')
+                     break
+                 elif dealCardSum >= userCardSum:
+                     print('あなたの負けです')
+                     break	
+
+    else:
+        break
+
     
     
     
