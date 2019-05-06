3d8534ec7c728b79b5a2bbe96331151dd55652cc
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-05-06 23:50:24 +0900

    add randomNumber

--- blackJack/main.py
+++ blackJack/main.py
@@ -1,13 +1,13 @@
+import random
 
-tramp = []
-for number in range(13):
-    tramp.append(number)
+upCardNum = random.randrange(1, 14) #ランダムな一枚にしたい
 
-upCardNum = 1 #ランダムな一枚にしたい
 
 print('私のアップカードは', upCardNum, 'です')
-userCard1 = 10
-userCard2 = 11 
+userCard1 = random.randrange(1, 14)
+userCard2 = random.randrange(1, 14) 
 print('あなたの手札：', userCard1, userCard2)
 
 userChoice = input('"stand"しますか？"hit"しますか？:')
+
+
